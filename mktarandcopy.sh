667f2fb52b122f8f29dc3d72ada279e941cdbeb8
Brad Brancke <bbrancke@srtrl.com>
2018-04-06 09:07:21 -0400

    commit Fri

--- mktarandcopy.sh
+++ mktarandcopy.sh
@@ -5,11 +5,11 @@ echo
 echo
 echo "*** COMPLETE, COPYING.... ***"
 #  cp nl80211test-1.0.0.tar.gz /home/office/yocto/poky/meta-shadowx/recipes-shadowx/nl80211/nl80211test-1.0.0/
-cp nl80211test-1.0.0.tar.gz /home/srtg/shx/poky/meta-shadowx/recipes-shadowx/nl80211/nl80211test-1.0.0/
+cp nl80211test-1.0.0.tar.gz /home/srtg/yocto/poky/meta-gumstix-extras/recipes-shadowx/nl80211/nl80211test-1.0.0
 
 echo "Local copy:"
 ls -l nl80211test-1.0.0.tar.gz
 
 echo "bitbake's copy:"
 #  ls -l /home/office/yocto/poky/meta-shadowx/recipes-shadowx/nl80211/nl80211test-1.0.0/
-ls -l /home/srtg/shx/poky/meta-shadowx/recipes-shadowx/nl80211/nl80211test-1.0.0/
+ls -l /home/srtg/yocto/poky/meta-gumstix-extras/recipes-shadowx/nl80211/nl80211test-1.0.0/
